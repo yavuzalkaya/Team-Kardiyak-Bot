3a6bfb27441aade840039ea454630f24b1423a04
yavuzalkaya <81232546+yavuzalkaya@users.noreply.github.com>
2021-03-26 15:14:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -270,7 +270,8 @@ client.on('message', message => {
 client.on('message', async message => {
   if (message.content.toLowerCase() === prefix + 'avatar') {
       const kanal = new MessageEmbed()
-      let user = message.mentions.users.first() || message.author;
+      const args = message.mentions.members.first() || message.author
+      const user = args[0]
       if (user) {
           const kanal = new MessageEmbed()
               .setTitle(`**${message.author.tag} AVATARINIZ**`)
