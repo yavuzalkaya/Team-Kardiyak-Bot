7b0dd159f1c25381a3a92f1fa7c56e923615e7a6
yavuzalkaya <81232546+yavuzalkaya@users.noreply.github.com>
2021-03-26 20:22:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -301,13 +301,14 @@ client.on('message', message => {
 client.on('message', async message => {
   if (message.content.startsWith() === prefix + 'avatar') {
       const kanal = new MessageEmbed()
-      let user = message.mentions.users.first()
+      let args = message.mentions.users.first() || message.author
+      const user = args[0]
       if (user) {
           const kanal = new MessageEmbed()
               .setTitle(`**${user.tag} AVATARINIZ**`)
               .setImage(user.displayAvatarURL({ dynamic: true, size:4096 }))
               .setFooter('Bot ile ilgili sorun ve sorularınızı discord sunucumuza gelerek yardım alabilirsiniz.')
-          message.channel.send(kanal)
+          message.channel.send(kanal);
       } else { 
           const kanal = new MessageEmbed()
               .setTitle(`**${message.author.tag} AVATARINIZ**`)
