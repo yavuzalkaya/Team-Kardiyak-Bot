966dd376e2f52e34ed9aebdd4b9b0bfbca54ac93
yavuzalkaya <81232546+yavuzalkaya@users.noreply.github.com>
2021-03-28 19:40:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ client.on('message', message => {
             .addField('Neden:', neden)
             .addField('Kickleyen Yetkili: ', `<@${message.author.id}>`)
             kanal.send(embed);
-            message.channel.send(<@member> + ' kişisi kicklenmiştir.');
+            message.channel.send(`<@member>` + ' kişisi kicklenmiştir.');
           })
           .catch(err => {
             message.channel.send('Bunu yapamam.');
@@ -107,7 +107,7 @@ if (message.content.startsWith(prefix + 'ban')) {
            .addField('Neden:', neden)
            .addField('Banlayan Yetkili:', `<@${message.author.id}>`)
            kanal.send(embed);
-           message.channel.send(<@member> + 'kişisi banlanmıştır.');
+           message.channel.send(`<@member>` + ' kişisi banlanmıştır.');
           })
           .catch(err => {
             message.channel.send('Bunu yapamam.');
